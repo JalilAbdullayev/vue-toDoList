0957a5d6d49313bc82ba20f9331151bb73c078a4
Jalil Abdullayev <abdullacelil00@gmail.com>
2024-12-05 23:15:04 +0400

    Create, delete & check tasks
    - Install Bootstrap, Bootstrap icons & Vue 3 Toastify
    - add checkbox to hide completed tasks

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,106 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script lang="ts">
+import {toast} from "vue3-toastify";
+
+export default {
+  data() {
+    return {
+      items: <ToDo[]>[],
+      newItem: '',
+      hiddenCompleted: false
+    }
+  },
+  methods: {
+    addItem() {
+      if(localStorage.getItem('items') !== null) {
+        this.items = JSON.parse(localStorage.getItem('items') ?? '[]');
+      } else {
+        localStorage.setItem('items', JSON.stringify(this.items));
+      }
+
+      (this.items as ToDo[]).push({
+        id: this.items.length + 1,
+        text: this.newItem,
+        done: false
+      });
+      localStorage.setItem('items', JSON.stringify(this.items));
+      this.newItem = '';
+      toast('Task added', {
+        type: 'success'
+      })
+    },
+    check(id: number) {
+      this.items.filter((item) => item.id === id)[0].done = !this.items.filter((item) => item.id === id)[0].done;
+      localStorage.setItem('items', JSON.stringify(this.items));
+      if(this.items.filter((item) => item.id === id)[0].done) {
+        toast('Task completed', {
+          type: 'success'
+        });
+      } else {
+        toast('Task not completed', {
+          type: 'warning'
+        });
+      }
+    },
+    remove(id: number) {
+      this.items = this.items.filter((item) => item.id !== id);
+      localStorage.setItem('items', JSON.stringify(this.items));
+      toast('Task removed', {
+        type: 'success'
+      });
+    },
+  },
+  mounted() {
+    if(localStorage.getItem('items') !== null) {
+      this.items = JSON.parse(localStorage.getItem('items') ?? '[]');
+    }
+  },
+  watch: {
+    hiddenCompleted() {
+      this.items = this.items.filter((item) => !item.done);
+      if(!this.hiddenCompleted) {
+        this.items = JSON.parse(localStorage.getItem('items') ?? '[]');
+      }
+    }
+  },
+}
+
+interface ToDo {
+  id: number;
+  text: string;
+  done: boolean;
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="container py-5">
+    <form @submit.prevent="addItem" class="row">
+      <div class="col-3 mb-3">
+        <input type="text" v-model="newItem" class="form-control"/>
+      </div>
+      <div class="col-2">
+        <input type="submit" class="btn btn-primary"/>
+      </div>
+      <div class="form-check">
+        <input type="checkbox" id="hide" v-model="hiddenCompleted" class="form-check-input"/>
+        <label for="hide" class="form-check-label">
+          Hide completed
+        </label>
+      </div>
+    </form>
+    <ul class="list-group mt-3 row">
+      <li v-for="item in items" :key="item.id"
+          class="list-group-item d-flex justify-content-between gap-3 align-items-center col-5">
+        <input type="checkbox" :checked="item.done" @change="check(item.id)" class="form-check-input"/>
+        <span>
+          {{ item.text }}
+        </span>
+        <button @click="remove(item.id)" class="btn btn-danger">
+          <i class="bi bi-trash3-fill"></i>
+        </button>
+      </li>
+    </ul>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import 'vue3-toastify/dist/index.css';
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import Vue3Toastify, {type ToastContainerOptions} from "vue3-toastify";
 
-createApp(App).mount('#app')
+createApp(App).use(Vue3Toastify, {
+  autoClose: 3000
+} as ToastContainerOptions).mount('#app')
